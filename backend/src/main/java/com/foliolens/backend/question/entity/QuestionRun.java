4cd88a6cbd78828a0f85c804d8bab4bbc0f28995
taiho0303 <taiho0303@gmail.com>
2026-08-05 13:25:24 +0900

    fix: QuestionRun entity 수정

--- backend/src/main/java/com/foliolens/backend/question/entity/QuestionRun.java
+++ backend/src/main/java/com/foliolens/backend/question/entity/QuestionRun.java
@@ -3,6 +3,7 @@ package com.foliolens.backend.question.entity;
 import java.time.Instant;
 import java.util.UUID;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
@@ -10,15 +11,6 @@ import com.foliolens.backend.domain.BaseCreatedEntity;
 import com.foliolens.backend.global.exception.ErrorCode;
 import com.foliolens.backend.question.RequestChannel;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import tools.jackson.databind.JsonNode;
@@ -32,32 +24,35 @@ import lombok.Builder;
 public class QuestionRun extends BaseCreatedEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    UUID id; //질문,처리상태,답변,처리상태를 모두 총괄하는 하나의 트랜잭션이 갖는 ID
+    private UUID id; //질문,처리상태,답변,처리상태를 모두 총괄하는 하나의 트랜잭션이 갖는 ID
 
     @Column(name="external_question_id",nullable = false)
-    String externalQuestionId; //평가 시스템이 보낸 Question ID
+    private String externalQuestionId; //평가 시스템이 보낸 Question ID
 
     @Column(name="question_text",nullable = false)
-    String questionText; //사용자가 작성한 프롬프트
+    private String questionText; //사용자가 작성한 프롬프트
 
+    @Enumerated(EnumType.STRING)
     @Column(name="channel",nullable = false)
-    RequestChannel channel;
+    private RequestChannel channel;
 
+    @Enumerated(EnumType.STRING)
     @Column(name="status",nullable = false)
-    QuestionRunStatus status;
+    private QuestionRunStatus status;
 
     @JdbcTypeCode(SqlTypes.JSON)
-    @Column(name="query_plan_json")
-    JsonNode queryPlanJson; //queryPlan을 Postegre에 JSONB 형태로 저장
+    @Column(name = "query_plan_json", columnDefinition = "jsonb")
+    private JsonNode queryPlanJson; //queryPlan을 Postegre에 JSONB 형태로 저장
 
-    @JoinColumn(name="answer_text")
-    String answerText; //해당 필드값은 계산,검증,금지 표현 필터링 등의 전처리를 거친 후의 HCX 답변 String
+    @Column(name="answer_text")
+    private String answerText; //해당 필드값은 계산,검증,금지 표현 필터링 등의 전처리를 거친 후의 HCX 답변 String
 
+    @Enumerated(EnumType.STRING)
     @Column(name="error_code")
-    ErrorCode errorCode;
+    private ErrorCode errorCode;
 
-    @Column(name="completed_at",updatable = false)
-    Instant completedAt;
+    @Column(name="completed_at")
+    private Instant completedAt;
 
     @PrePersist
     @PreUpdate
