6945ca209a51463599f6395b25af352fee96f6ac
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2026-08-05 13:30:58 +0900

    fix)code alignment

--- backend/src/main/java/com/foliolens/backend/question/plan/QuestionPlan.java
+++ backend/src/main/java/com/foliolens/backend/question/plan/QuestionPlan.java
@@ -9,10 +9,10 @@ import com.foliolens.backend.company.domain.Company;
 // Spring 검증을 통과한 경우에만 생성 必
 // AI가 만든 QuestionPlanCandidate에서 검증이 통과되어 허가된 Plan
 public record QuestionPlan(Long schemaVersion,
-        List<Company> companies,
-        LocalDateTime time,
-        Set<IntentType> intents,
-        //List<> interestProfiles,
-        List<PlanStep> steps,
-        List<String> warnings) {
+                List<Company> companies,
+                LocalDateTime time,
+                Set<IntentType> intents,
+                // List<> interestProfiles,
+                List<PlanStep> steps,
+                List<String> warnings) {
 }
--- backend/src/main/java/com/foliolens/backend/question/plan/QuestionPlanCandidate.java
+++ backend/src/main/java/com/foliolens/backend/question/plan/QuestionPlanCandidate.java
@@ -6,12 +6,11 @@ import java.util.Set;
 
 // 사용자의 프롬프트 요구사항에 맞게끔 AI가 출력한 미검증 공시 검색 계획
 public record QuestionPlanCandidate(Long schemaVersion,
-    Set<IntentType> intents,
-    String companiesMention, 
-    LocalDate from,
-    LocalDate to,
-    Set<String> interestCodes,
-    List<PlanStep> steps,
-    List<String> ambiguities
-) {
+        Set<IntentType> intents,
+        String companiesMention,
+        LocalDate from,
+        LocalDate to,
+        Set<String> interestCodes,
+        List<PlanStep> steps,
+        List<String> ambiguities) {
 }
